84031f4f065201ceefb05319cd2947972ae61e48
benchook <benzionfisher@gmail.com>
2020-09-01 13:58:05 +0300

    Update Main.java
    10

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,10 +3,6 @@ public class Main {
 	public static void main(String [] strings) {
 		System.out.println("Hello world");
 		//maybe dotan added this comment?
-<<<<<<< HEAD
-		//my addition 8
-=======
-		//my addition 7
->>>>>>> branch 'master' of https://github.com/benchook/ben_repository/
+		//my addition 10
 	}
 }
