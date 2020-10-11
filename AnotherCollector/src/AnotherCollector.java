3567cfb12e015ce81d8a783f940fad877003cd77
benchook <benzionfisher@gmail.com>
2020-10-11 20:30:13 +0300

    Update AnotherCollector.java
    another change from remote

--- AnotherCollector/src/AnotherCollector.java
+++ AnotherCollector/src/AnotherCollector.java
@@ -3,5 +3,6 @@ public class AnotherCollector {
 	public static void main(String [] strs) {
 		System.out.println("AnotherCollector branch1 and branch2");
 		System.out.println("AnotherCollector another change");
+		System.out.println("AnotherCollector another change from remote");
 	}
 }
